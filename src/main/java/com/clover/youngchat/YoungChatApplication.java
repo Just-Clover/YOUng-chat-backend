b89df3c5a6b4c9c5c938c1b7ffb3a02bdc0cfd4a
junxtar <aa1870088@naver.com>
2024-01-08 11:37:45 +0900

    Test: buildTest

--- src/main/java/com/clover/youngchat/YoungChatApplication.java
+++ src/main/java/com/clover/youngchat/YoungChatApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YoungChatApplication {
 
     public static void main(String[] args) {
+        long a = "test";
         SpringApplication.run(YoungChatApplication.class, args);
     }
 
