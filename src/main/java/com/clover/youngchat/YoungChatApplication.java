d39298ab2a7c025632cdd048b0c364984a5b6718
junxtar <aa1870088@naver.com>
2024-01-08 11:43:58 +0900

    [#6] Test: test code 삭제

--- src/main/java/com/clover/youngchat/YoungChatApplication.java
+++ src/main/java/com/clover/youngchat/YoungChatApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YoungChatApplication {
 
     public static void main(String[] args) {
-        long a = "test";
         SpringApplication.run(YoungChatApplication.class, args);
     }
 
