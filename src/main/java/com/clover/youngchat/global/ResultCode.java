6fe519397fedf8f5cc5e352a59accdd3269fd601
yongcowhee <yongcowhee@gmail.com>
2024-01-08 15:40:23 +0900

    [#12] Feat: ResultCode 추가

---
+++ src/main/java/com/clover/youngchat/global/ResultCode.java
@@ -0,0 +1,25 @@
+package com.clover.youngchat.global;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum ResultCode {
+    // 성공 0번대
+    SUCCESS(HttpStatus.OK, 0, "정상 처리되었습니다."),
+
+    // 글로벌 1000번대
+    SYSTEM_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 1000, "알 수 없는 에러가 발생했습니다."),
+    ACCESS_DENY(HttpStatus.FORBIDDEN, 1001, "접근 권한이 없습니다.");
+
+    // 유저 2000번대
+
+    // 채팅방 3000번대
+
+    // 채팅 4000번대
+
+    private final HttpStatus status;
+    private final Integer code;
+    private final String message;
+
+}
