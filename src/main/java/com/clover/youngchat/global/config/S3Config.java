824a250064a62d483368e6d743ebcf157b86fd95
yongcowhee <yongcowhee@gmail.com>
2024-01-09 16:13:34 +0900

    [#30] Feat: S3 Config 추가

---
+++ src/main/java/com/clover/youngchat/global/config/S3Config.java
@@ -0,0 +1,32 @@
+package com.clover.youngchat.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.s3.credentials.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.s3.credentials.secretKey}")
+    private String secretKey;
+    @Value("${cloud.aws.s3.region}")
+    private String region;
+
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
+    }
+}
