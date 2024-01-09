0bb25aad3e5d5b080cc49c280b771f5e25e29962
junxtar <aa1870088@naver.com>
2024-01-09 16:08:57 +0900

    [#27] Test: FriendController @Mockbean 추가

--- src/test/java/com/clover/youngchat/domain/friend/controller/FriendControllerTest.java
+++ src/test/java/com/clover/youngchat/domain/friend/controller/FriendControllerTest.java
@@ -5,12 +5,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.clover.youngchat.domain.BaseMvcTest;
+import com.clover.youngchat.domain.friend.service.FriendService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 
-@WebMvcTest(controllers = {FriendController.class})
+@WebMvcTest(controllers = FriendController.class)
 public class FriendControllerTest extends BaseMvcTest {
+
+    @MockBean
+    private FriendService friendService;
 
     @Test
     @DisplayName("친구추가 테스트")
@@ -20,7 +26,8 @@ public class FriendControllerTest extends BaseMvcTest {
 
         // when - then
         mockMvc.perform(post("/api/v1/friends/" + friendId)
-                .principal(mockPrincipal))
+                .principal(mockPrincipal)
+                .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andDo(print());
     }
