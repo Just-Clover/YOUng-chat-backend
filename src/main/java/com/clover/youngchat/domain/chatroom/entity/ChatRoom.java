9d79bd59906c3a0f761999ab4a131107c72db6a5
yongcowhee <yongcowhee@gmail.com>
2024-01-10 16:26:34 +0900

    [#39] Feat: ChatRoom entity 구현

---
+++ src/main/java/com/clover/youngchat/domain/chatroom/entity/ChatRoom.java
@@ -0,0 +1,28 @@
+package com.clover.youngchat.domain.chatroom.entity;
+
+import com.clover.youngchat.domain.model.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Builder
+    private ChatRoom(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
