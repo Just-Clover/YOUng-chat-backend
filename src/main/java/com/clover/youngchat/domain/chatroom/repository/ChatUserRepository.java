90c906e4cd812830679514e7e105cb1fc1951154
yongcowhee <yongcowhee@gmail.com>
2024-01-10 17:08:45 +0900

    [#39] Feat: ChatUserRepository 생성

---
+++ src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatUserRepository.java
@@ -0,0 +1,9 @@
+package com.clover.youngchat.domain.chatroom.repository;
+
+import com.clover.youngchat.domain.chatroom.entity.ChatUser;
+import org.springframework.data.repository.RepositoryDefinition;
+
+@RepositoryDefinition(domainClass = ChatUser.class, idClass = Long.class)
+public interface ChatUserRepository {
+
+}
