17d3df0b7bc052e00b942ee5549833d73d5c00dd
seastamp <seastamp10@gmail.com>
2024-01-12 01:53:02 +0900

    [#58] Feat: Blacklist Service 구현

---
+++ src/main/java/com/clover/youngchat/domain/auth/service/BlacklistService.java
@@ -0,0 +1,25 @@
+package com.clover.youngchat.domain.auth.service;
+
+import com.clover.youngchat.domain.auth.entity.Blacklist;
+import com.clover.youngchat.domain.auth.repository.BlacklistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlacklistService {
+
+    private final BlacklistRepository blacklistRepository;
+
+    public void addTokenToBlacklist(String accessToken) {
+        Blacklist blacklist = Blacklist.builder()
+            .accessToken(accessToken)
+            .build();
+        blacklistRepository.save(blacklist);
+    }
+
+    public boolean isTokenBlackListed(String accessToken) {
+        return blacklistRepository.existById(accessToken);
+    }
+
+} 
