fc2d8e2f95236fa6f628cf1468d78e54bc219288
seastamp <seastamp10@gmail.com>
2024-01-12 01:52:53 +0900

    [#58] Feat: Blacklist Repository 구현

---
+++ src/main/java/com/clover/youngchat/domain/auth/repository/BlacklistRepository.java
@@ -0,0 +1,12 @@
+package com.clover.youngchat.domain.auth.repository;
+
+import com.clover.youngchat.domain.auth.entity.Blacklist;
+import org.springframework.data.repository.RepositoryDefinition;
+
+@RepositoryDefinition(domainClass = Blacklist.class, idClass = String.class)
+public interface BlacklistRepository {
+
+    Blacklist save(Blacklist blacklist);
+
+    boolean existById(String accessToken);
+}
