ca76267d1cbcf22ed5f6744dac30b0ea8ad7744b
seastamp <seastamp10@gmail.com>
2024-01-11 19:41:58 +0900

    [#51] Feat: 채팅 삭제기능 Controller 구현

--- src/main/java/com/clover/youngchat/domain/chat/controller/ChatController.java
+++ src/main/java/com/clover/youngchat/domain/chat/controller/ChatController.java
@@ -2,11 +2,13 @@ package com.clover.youngchat.domain.chat.controller;
 
 import com.clover.youngchat.domain.chat.dto.request.ChatCreateReq;
 import com.clover.youngchat.domain.chat.dto.response.ChatCreateRes;
+import com.clover.youngchat.domain.chat.dto.response.ChatDeleteRes;
 import com.clover.youngchat.domain.chat.service.ChatService;
 import com.clover.youngchat.global.response.RestResponse;
 import com.clover.youngchat.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +30,14 @@ public class ChatController {
     ) {
         return RestResponse.success(
             chatService.createChat(chatRoomId, req, userDetails.getUser().getId()));
+    }
+
+    @DeleteMapping("/{chatId}")
+    public RestResponse<ChatDeleteRes> deleteChat(
+        @PathVariable Long chatRoomId, @PathVariable Long chatId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return RestResponse.success(
+            chatService.deleteChat(chatRoomId, chatId, userDetails.getUser().getId()));
     }
 
 }
