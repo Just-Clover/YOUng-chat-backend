8e4175e21d26cdbe1ba82c75fefe216988cc8a70
yongcowhee <yongcowhee@gmail.com>
2024-01-12 18:31:27 +0900

    [#61] Test: 채팅방 생성 실패: 존재하지 않는 유저 서비스 테스트 추가

--- src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
+++ src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
@@ -1,5 +1,7 @@
 package com.clover.youngchat.domain.chatRoom.service;
 
+import static com.clover.youngchat.global.exception.ResultCode.NOT_FOUND_USER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -15,9 +17,12 @@ import com.clover.youngchat.domain.chatroom.repository.ChatRoomRepository;
 import com.clover.youngchat.domain.chatroom.repository.ChatRoomUserRepository;
 import com.clover.youngchat.domain.chatroom.service.ChatRoomService;
 import com.clover.youngchat.domain.user.repository.UserRepository;
+import com.clover.youngchat.global.exception.GlobalException;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -48,21 +53,42 @@ public class ChatRoomServiceTest {
             .build();
     }
 
-    @Test
-    @DisplayName("채팅방 생성 성공")
-    void createChatRoomSuccess() {
-        ChatRoomCreateReq req = ChatRoomCreateReq.builder()
-            .title(TEST_CHAT_ROOM_TITLE)
-            .friendId(ANOTHER_TEST_USER_ID)
-            .build();
+    @Nested
+    @DisplayName("채팅방 생성")
+    class createChatRoom {
 
-        given(userRepository.findById(anyLong())).willReturn(Optional.of(TEST_USER));
+        @Test
+        @DisplayName("채팅방 생성 성공")
+        void createChatRoomSuccess() {
+            ChatRoomCreateReq req = ChatRoomCreateReq.builder()
+                .title(TEST_CHAT_ROOM_TITLE)
+                .friendId(ANOTHER_TEST_USER_ID)
+                .build();
 
-        chatRoomService.createChatRoom(req, TEST_USER);
+            given(userRepository.findById(anyLong())).willReturn(Optional.of(TEST_USER));
 
-        verify(userRepository, times(1)).findById(anyLong());
-        verify(chatRoomRepository, times(1)).save(any());
-        verify(chatRoomUserRepository, times(2)).save(any());
+            chatRoomService.createChatRoom(req, TEST_USER);
+
+            verify(userRepository, times(1)).findById(anyLong());
+            verify(chatRoomRepository, times(1)).save(any());
+            verify(chatRoomUserRepository, times(2)).save(any());
+        }
+
+        @Test
+        @DisplayName("채팅방 생성 실패 : 존재하지 않는 유저")
+        void createChatRoomFail_NotFoundUser() {
+            ChatRoomCreateReq req = ChatRoomCreateReq.builder()
+                .title(TEST_CHAT_ROOM_TITLE)
+                .friendId(ANOTHER_TEST_USER_ID)
+                .build();
+
+            given(userRepository.findById(anyLong())).willReturn(Optional.empty());
+
+            GlobalException exception = Assertions.assertThrows(GlobalException.class,
+                () -> chatRoomService.createChatRoom(req, TEST_USER));
+
+            assertThat(exception.getResultCode().getMessage()).isEqualTo(
+                NOT_FOUND_USER.getMessage());
+        }
     }
-
 }
