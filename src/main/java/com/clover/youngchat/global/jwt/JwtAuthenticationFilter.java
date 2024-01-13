65177700daab97e5c389e84686f4f028fbee17ff
junxtar <aa1870088@naver.com>
2024-01-13 21:45:31 +0900

    [#73] Feat: 로그인 실패 로직 추가 및 로직 개선

--- src/main/java/com/clover/youngchat/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/clover/youngchat/global/jwt/JwtAuthenticationFilter.java
@@ -1,34 +1,38 @@
 package com.clover.youngchat.global.jwt;
 
+import static com.clover.youngchat.global.exception.ResultCode.NOT_FOUND_USER;
+
 import com.clover.youngchat.domain.user.dto.request.UserLoginReq;
 import com.clover.youngchat.domain.user.entity.User;
-import com.clover.youngchat.domain.user.service.UserService;
 import com.clover.youngchat.global.redis.RedisUtil;
+import com.clover.youngchat.global.response.RestResponse;
 import com.clover.youngchat.global.security.UserDetailsImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j(topic = "로그인 및 JWT 생성")
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    @Autowired
-    private UserService userService;
+    public final Integer REFRESH_TOKEN_TIME = 60 * 24 * 14;
     private final JwtUtil jwtUtil;
     private final RedisUtil redisUtil;
-    public final Integer REFRESH_TOKEN_TIME = 60 * 24 * 14;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, RedisUtil redisUtil) {
-        this.jwtUtil = jwtUtil;
-        this.redisUtil = redisUtil;
+    @PostConstruct
+    public void setup() {
         setFilterProcessesUrl("/api/v1/users/login");
     }
 
@@ -66,6 +70,25 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         refreshToken = refreshToken.split(" ")[1].trim();
 
-        redisUtil.set(refreshToken, user.getId(), REFRESH_TOKEN_TIME);
+        redisUtil.set(refreshToken, user.getEmail(), REFRESH_TOKEN_TIME);
     }
+
+    @Override
+    protected void unsuccessfulAuthentication(
+        HttpServletRequest request, HttpServletResponse response, AuthenticationException failed)
+        throws IOException {
+        response.setStatus(NOT_FOUND_USER.getStatus().value());
+        settingResponse(response, RestResponse.error(NOT_FOUND_USER));
+    }
+
+    private void settingResponse(HttpServletResponse response, RestResponse<?> res)
+        throws IOException {
+
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        String result = objectMapper.writeValueAsString(res);
+        response.getWriter().write(result);
+    }
+
 }
