a271b3c94708a366867fcaa59cdaf5eea3b6d782
YONG-COW-HEE <yongcowhee@gmail.com>
2024-01-14 17:32:24 +0900

    [#78] Test: 채팅방 나가기 서비스 테스트 성공 추가

--- src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
+++ src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
@@ -7,7 +7,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static test.ChatRoomTest.TEST_CHAT_ROOM_TITLE;
+import static test.ChatRoomTest.*;
+import static test.ChatRoomUserTest.TEST_CHAT_ROOM_USER;
 import static test.UserTest.ANOTHER_TEST_USER_ID;
 import static test.UserTest.TEST_USER;
 
@@ -16,6 +17,7 @@ import com.clover.youngchat.domain.chatroom.entity.ChatRoom;
 import com.clover.youngchat.domain.chatroom.repository.ChatRoomRepository;
 import com.clover.youngchat.domain.chatroom.repository.ChatRoomUserRepository;
 import com.clover.youngchat.domain.chatroom.service.ChatRoomService;
+import com.clover.youngchat.domain.user.entity.User;
 import com.clover.youngchat.domain.user.repository.UserRepository;
 import com.clover.youngchat.global.exception.GlobalException;
 import java.util.Optional;
@@ -28,6 +30,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 public class ChatRoomServiceTest {
@@ -46,11 +49,23 @@ public class ChatRoomServiceTest {
 
     private ChatRoom chatRoom;
 
+    private User user;
+
     @BeforeEach
     void setup() {
         chatRoom = ChatRoom.builder()
             .title(TEST_CHAT_ROOM_TITLE)
             .build();
+        ReflectionTestUtils.setField(chatRoom, "id", 1L);
+
+        user = User.builder()
+            .username(TEST_USER_NAME)
+            .email(TEST_USER_EMAIL)
+            .profileImage(TEST_USER_PROFILE_IMAGE)
+            .password(TEST_USER_PASSWORD)
+            .build();
+
+        ReflectionTestUtils.setField(user, "id", 1L);
     }
 
     @Nested
@@ -89,6 +104,23 @@ public class ChatRoomServiceTest {
 
             assertThat(exception.getResultCode().getMessage()).isEqualTo(
                 NOT_FOUND_USER.getMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("채팅방 나가기")
+    class leaveChatRoom {
+        @Test
+        @DisplayName("성공")
+        void leaveChatRoomSuccess(){
+            given(chatRoomRepository.existsById(anyLong())).willReturn(true);
+            given(chatRoomUserRepository.findByChatRoom_IdAndUser_Id(anyLong(), anyLong())).willReturn(Optional.of(TEST_CHAT_ROOM_USER));
+
+            chatRoomService.leaveChatRoom(TEST_CHAT_ROOM_ID, user);
+
+            verify(chatRoomRepository,times(1)).existsById(anyLong());
+            verify(chatRoomUserRepository, times(1)).findByChatRoom_IdAndUser_Id(anyLong(),anyLong());
+            verify(chatRoomUserRepository,times(1)).delete(any());
         }
     }
 }
