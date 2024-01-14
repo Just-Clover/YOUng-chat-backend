b0a6738cc1e4b99c58dfc856334e6587d6ed92e7
YONG-COW-HEE <yongcowhee@gmail.com>
2024-01-14 17:46:45 +0900

    [#78] Test: 채팅방 나가기 서비스 테스트 실패: 채팅방에 속한 유저가 아닐 경우 추가

--- src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
+++ src/test/java/com/clover/youngchat/domain/chatRoom/service/ChatRoomServiceTest.java
@@ -1,14 +1,21 @@
 package com.clover.youngchat.domain.chatRoom.service;
 
+import static com.clover.youngchat.global.exception.ResultCode.ACCESS_DENY;
 import static com.clover.youngchat.global.exception.ResultCode.NOT_FOUND_CHATROOM;
 import static com.clover.youngchat.global.exception.ResultCode.NOT_FOUND_USER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static test.ChatRoomTest.*;
+import static test.ChatRoomTest.TEST_CHAT_ROOM_ID;
+import static test.ChatRoomTest.TEST_CHAT_ROOM_TITLE;
+import static test.ChatRoomTest.TEST_USER_EMAIL;
+import static test.ChatRoomTest.TEST_USER_NAME;
+import static test.ChatRoomTest.TEST_USER_PASSWORD;
+import static test.ChatRoomTest.TEST_USER_PROFILE_IMAGE;
 import static test.ChatRoomUserTest.TEST_CHAT_ROOM_USER;
 import static test.UserTest.ANOTHER_TEST_USER_ID;
 import static test.UserTest.TEST_USER;
@@ -22,7 +29,6 @@ import com.clover.youngchat.domain.user.entity.User;
 import com.clover.youngchat.domain.user.repository.UserRepository;
 import com.clover.youngchat.global.exception.GlobalException;
 import java.util.Optional;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -100,7 +106,7 @@ public class ChatRoomServiceTest {
 
             given(userRepository.findById(anyLong())).willReturn(Optional.empty());
 
-            GlobalException exception = Assertions.assertThrows(GlobalException.class,
+            GlobalException exception = assertThrows(GlobalException.class,
                 () -> chatRoomService.createChatRoom(req, TEST_USER));
 
             assertThat(exception.getResultCode().getMessage()).isEqualTo(
@@ -129,10 +135,22 @@ public class ChatRoomServiceTest {
         void leaveChatRoomFail_NotFoundChatRoom(){
             given(chatRoomRepository.existsById(anyLong())).willReturn(false);
 
-            GlobalException exception = Assertions.assertThrows(GlobalException.class,
+            GlobalException exception = assertThrows(GlobalException.class,
                 ()-> chatRoomService.leaveChatRoom(TEST_CHAT_ROOM_ID, user));
 
             assertThat(exception.getResultCode().getMessage()).isEqualTo(NOT_FOUND_CHATROOM.getMessage());
+        }
+
+        @Test
+        @DisplayName("실패 : 채팅방에 속한 유저가 아닐 경우")
+        void leaveChatRoomFail_AccessDeny(){
+            given(chatRoomRepository.existsById(anyLong())).willReturn(true);
+            given(chatRoomUserRepository.findByChatRoom_IdAndUser_Id(anyLong(), anyLong())).willReturn(Optional.empty());
+
+            GlobalException exception = assertThrows(GlobalException.class,
+                ()-> chatRoomService.leaveChatRoom(TEST_CHAT_ROOM_ID, user));
+
+            assertThat(exception.getResultCode().getMessage()).isEqualTo(ACCESS_DENY.getMessage());
         }
     }
 }
