c8f2aad073d7c472261cf0cab4ad0e31318562bf
seastamp <seastamp10@gmail.com>
2024-01-14 16:01:09 +0900

    [#75] Refactor: Chat에 생성시간 필드 삭제후 BaseEntity 상속받도록 수정

--- src/main/java/com/clover/youngchat/domain/chat/entity/Chat.java
+++ src/main/java/com/clover/youngchat/domain/chat/entity/Chat.java
@@ -1,10 +1,9 @@
 package com.clover.youngchat.domain.chat.entity;
 
 import com.clover.youngchat.domain.chatroom.entity.ChatRoom;
+import com.clover.youngchat.domain.model.BaseEntity;
 import com.clover.youngchat.domain.user.entity.User;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,20 +11,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Table(name = "chat")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-public class Chat {
+public class Chat extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,10 +35,6 @@ public class Chat {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chatroom_id", nullable = false, updatable = false)
     private ChatRoom chatRoom;
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
 
     // TODO: senderName 따로뺐을때 성능차이 어떻게 나는지 확인해보기
 
