f97f3b9d373b9e4b48f0f15732e8a890300ee880
junxtar <aa1870088@naver.com>
2024-01-17 00:06:30 +0900

    [#86] Refactor: 친구 ID 값 반환 추가

--- src/main/java/com/clover/youngchat/domain/friend/dto/response/FriendGetListRes.java
+++ src/main/java/com/clover/youngchat/domain/friend/dto/response/FriendGetListRes.java
@@ -9,17 +9,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FriendGetListRes {
 
+    private Long userId;
     private String username;
     private String profileImage;
 
     @Builder
-    private FriendGetListRes(String username, String profileImage) {
+    private FriendGetListRes(Long userId, String username, String profileImage) {
+        this.userId = userId;
         this.username = username;
         this.profileImage = profileImage;
     }
 
-    public static FriendGetListRes to(String username, String profileImage) {
+    public static FriendGetListRes to(Long userId, String username, String profileImage) {
         return FriendGetListRes.builder()
+            .userId(userId)
             .username(username)
             .profileImage(profileImage)
             .build();
