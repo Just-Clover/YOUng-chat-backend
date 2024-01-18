07d7430ced7160985a93f8e872e96025df20d922
junxtar <aa1870088@naver.com>
2024-01-18 13:00:41 +0900

    [#98] Feat: 사용자 검색 API 기능 구현

--- src/main/java/com/clover/youngchat/domain/user/constant/UserConstant.java
+++ src/main/java/com/clover/youngchat/domain/user/constant/UserConstant.java
@@ -8,6 +8,7 @@ public class UserConstant {
     public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$";
 
     // 메시지
+    public static final String EMAIL_AUTHENTICATION = "YOUngChat! [이메일 인증]";
     public static final String EMAIL_MESSAGE = "올바른 이메일 형식이 아닙니다.";
     public static final String USERNAME_MESSAGE = "username은 영소문자,한글 4글자 이상 10글자 이하입니다";
     public static final String PASSWORD_MESSAGE = "비밀번호는 영소문자, 대문자, 특수문자, 숫자를 포함한 8글자 이상 15글자 이하입니다.";
