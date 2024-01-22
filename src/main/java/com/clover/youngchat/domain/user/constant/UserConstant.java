e0f3a48811b0384ca5c173c3b7d6584bdc2a6fa2
junxtar <aa1870088@naver.com>
2024-01-22 22:55:21 +0900

    [#134] Fix: 정규식 재작성

--- src/main/java/com/clover/youngchat/domain/user/constant/UserConstant.java
+++ src/main/java/com/clover/youngchat/domain/user/constant/UserConstant.java
@@ -3,7 +3,7 @@ package com.clover.youngchat.domain.user.constant;
 public class UserConstant {
 
     // 정규식
-    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+    public static final String EMAIL_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$";
     public static final String USERNAME_REGEX = "^[a-z가-힣]{4,10}$";
     public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$";
 
