839e6dea48f1f5798471707c855fbe87f64734b2
junxtar <aa1870088@naver.com>
2024-01-26 15:59:18 +0900

    [#175] Refactor: RabbitMq 적용

--- src/main/java/com/clover/youngchat/domain/chat/controller/ChatController.java
+++ src/main/java/com/clover/youngchat/domain/chat/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.clover.youngchat.domain.chat.controller;
 
 import com.clover.youngchat.domain.chat.dto.request.ChatCreateReq;
 import com.clover.youngchat.domain.chat.dto.response.ChatDeleteRes;
-import com.clover.youngchat.domain.chat.dto.response.ChatRes;
 import com.clover.youngchat.domain.chat.service.ChatService;
 import com.clover.youngchat.global.response.RestResponse;
 import com.clover.youngchat.global.security.UserDetailsImpl;
@@ -10,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,11 +22,10 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    @SendTo("/sub/chat-rooms/{chatRoomId}/chats")
-    @MessageMapping("/chat-rooms/{chatRoomId}/chats")
-    public RestResponse<ChatRes> sendMessage(@DestinationVariable Long chatRoomId,
+    @MessageMapping("chat-rooms.{chatRoomId}")
+    public void sendMessage(@DestinationVariable Long chatRoomId,
         @Payload ChatCreateReq chatCreateReq) {
-        return RestResponse.success(chatService.sendMessage(chatRoomId, chatCreateReq));
+        chatService.sendMessage(chatRoomId, chatCreateReq);
     }
 
     @DeleteMapping("/{chatId}")
