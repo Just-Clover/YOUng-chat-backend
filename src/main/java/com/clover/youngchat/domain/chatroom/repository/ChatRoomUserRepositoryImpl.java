b394a01c07df1d2c1c745023caa6739d0a203b29
seastamp <seastamp10@gmail.com>
2024-01-30 23:56:29 +0900

    [#148] Refactor: 성능 개선을 위해 생성된 1:1채팅방이 있는지 확인하는 QueryDSL을 JPQL로 수정

--- src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryImpl.java
+++ src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryImpl.java
@@ -1,10 +1,7 @@
 package com.clover.youngchat.domain.chatroom.repository;
 
-import com.clover.youngchat.domain.chatroom.entity.ChatRoom;
 import com.clover.youngchat.domain.chatroom.entity.QChatRoomUser;
 import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
@@ -14,31 +11,6 @@ import lombok.RequiredArgsConstructor;
 public class ChatRoomUserRepositoryImpl implements ChatRoomUserRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-
-    @Override
-    public Optional<ChatRoom> findChatRoomIdByOnlyTwoUsers(final Long userId, final Long friendId) {
-        QChatRoomUser chatRoomUser = QChatRoomUser.chatRoomUser;
-        QChatRoomUser chatRoomUserSub = new QChatRoomUser("chatRoomUserSub");
-
-        // 서브쿼리: 두 사용자가 모두 있는 채팅방 ID 찾기
-        JPQLQuery<Long> subQuery = JPAExpressions
-            .select(chatRoomUserSub.chatRoom.id)
-            .from(chatRoomUserSub)
-            .where(chatRoomUserSub.user.id.in(userId, friendId))
-            .groupBy(chatRoomUserSub.chatRoom.id)
-            .having(chatRoomUserSub.user.id.count().eq(2L));
-
-        // 메인 쿼리: 서브쿼리 결과를 이용하여 ChatRoom 찾기
-        ChatRoom chatRoom = queryFactory
-            .select(chatRoomUser.chatRoom)
-            .from(chatRoomUser)
-            .where(chatRoomUser.chatRoom.id.in(subQuery))
-            .groupBy(chatRoomUser.chatRoom.id)
-            .having(chatRoomUser.user.id.count().eq(2L))
-            .fetchOne();
-
-        return Optional.ofNullable(chatRoom);
-    }
 
     @Override
     public Optional<List<Long>> getOtherUsersInChatRoom(Long chatRoomId, Long userId) {
