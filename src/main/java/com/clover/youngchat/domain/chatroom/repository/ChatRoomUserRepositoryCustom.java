a6a368a719ec3d757b42c0aa42b4fe2ce92fb0f4
yongcowhee <yongcowhee@gmail.com>
2024-01-31 14:31:25 +0900

    [#220] Feat: 채팅방 목록 조회 페이지네이션 인터페이스에 추가

--- src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryCustom.java
+++ src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryCustom.java
@@ -1,11 +1,15 @@
 package com.clover.youngchat.domain.chatroom.repository;
 
+import com.clover.youngchat.domain.chatroom.dto.response.ChatRoomAndLastChatGetRes;
 import com.clover.youngchat.domain.chatroom.entity.ChatRoom;
 import java.util.Optional;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomUserRepositoryCustom {
 
     Optional<ChatRoom> findChatRoomIdByOnlyTwoUsers(Long userId1, Long userId2);
+
+    Slice<ChatRoomAndLastChatGetRes> findChatRoomsAndLastChatByUserId(Long userId, Long cursorChatRoomId, int limitSize);
 }
