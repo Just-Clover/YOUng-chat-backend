5277e2d2b6a0b5acbaadf485988f9932e725edd6
yongcowhee <yongcowhee@gmail.com>
2024-01-31 14:32:46 +0900

    [#220] Feat: 채팅방 목록 조회 QueryDSL 작성

--- src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryImpl.java
+++ src/main/java/com/clover/youngchat/domain/chatroom/repository/ChatRoomUserRepositoryImpl.java
@@ -1,11 +1,19 @@
 package com.clover.youngchat.domain.chatroom.repository;
 
+import com.clover.youngchat.domain.chat.entity.QChat;
+import com.clover.youngchat.domain.chatroom.dto.response.ChatRoomAndLastChatGetRes;
+import com.clover.youngchat.domain.chatroom.entity.QChatRoom;
 import com.clover.youngchat.domain.chatroom.entity.QChatRoomUser;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
 @RequiredArgsConstructor
 public class ChatRoomUserRepositoryImpl implements ChatRoomUserRepositoryCustom {
@@ -23,5 +31,50 @@ public class ChatRoomUserRepositoryImpl implements ChatRoomUserRepositoryCustom 
                 .groupBy(chatRoomUser.user.id)
                 .having(chatRoomUser.user.id.notIn(userId))
                 .fetch());
+    }
+
+    @Override
+    public Slice<ChatRoomAndLastChatGetRes> findChatRoomsAndLastChatByUserId(Long userId,
+        Long cursorChatRoomId,
+        int limitSize) {
+        List<ChatRoomAndLastChatGetRes> resList = queryChatRooms(userId, cursorChatRoomId,
+            limitSize);
+        return null;
+    }
+
+    private List<ChatRoomAndLastChatGetRes> queryChatRooms(Long userId, Long cursorChatRoomId,
+        int limitSize) {
+        QChatRoomUser qChatRoomUser = QChatRoomUser.chatRoomUser;
+        QChatRoom qChatRoom = QChatRoom.chatRoom;
+        QChat qChat = QChat.chat;
+
+        return queryFactory
+            .select(
+                Projections.constructor(ChatRoomAndLastChatGetRes.class,
+                    qChatRoom.id,
+                    qChatRoom.title,
+                    qChat.message,
+                    qChat.createdAt,
+                    qChat.isDeleted))
+            .from(qChatRoomUser)
+            .join(qChatRoom).on(qChatRoomUser.chatRoom.id.eq(qChatRoom.id))
+            .join(qChat).on(qChat.chatRoom.id.eq(qChatRoom.id))
+            .where(qChatRoomUser.user.id.eq(userId))
+            .where(Expressions.list(qChatRoom.id, qChat.createdAt).in(
+                JPAExpressions
+                    .select(qChat.chatRoom.id, qChat.createdAt.max())
+                    .from(qChat)
+                    .groupBy(qChat.chatRoom.id)))
+            .fetch();
+    }
+
+
+    private Slice<ChatRoomAndLastChatGetRes> createSlice(List<ChatRoomAndLastChatGetRes> res,
+        int limitSize) {
+        boolean hasNext = res.size() > limitSize;
+        if (hasNext) {
+            res.remove(0);
+        }
+        return new SliceImpl<>(res, PageRequest.of(0, limitSize), hasNext);
     }
 }
