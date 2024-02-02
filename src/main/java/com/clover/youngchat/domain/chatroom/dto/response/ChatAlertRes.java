9cbb561205fdb9d586aae20e9f8ca7e0a9d07488
junxtar <aa1870088@naver.com>
2024-02-02 20:02:25 +0900

    [#244] Refactor: 작업사항                   알림 반환 데이터에 ChatRoomId 추가

--- src/main/java/com/clover/youngchat/domain/chatroom/dto/response/ChatAlertRes.java
+++ src/main/java/com/clover/youngchat/domain/chatroom/dto/response/ChatAlertRes.java
@@ -9,23 +9,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatAlertRes {
 
+    private Long chatRoomId;
     private String chatRoomName;
     private String username;
     private String profileImage;
     private String message;
 
     @Builder
-    private ChatAlertRes(String chatRoomName, String username, String profileImage,
+    private ChatAlertRes(Long chatRoomId, String chatRoomName, String username, String profileImage,
         String message) {
+        this.chatRoomId = chatRoomId;
         this.chatRoomName = chatRoomName;
         this.username = username;
         this.profileImage = profileImage;
         this.message = message;
     }
 
-    public static ChatAlertRes to(String chatRoomName, String username, String profileImage,
+    public static ChatAlertRes to(Long chatRoomId, String chatRoomName, String username,
+        String profileImage,
         String message) {
         return ChatAlertRes.builder()
+            .chatRoomId(chatRoomId)
             .chatRoomName(chatRoomName)
             .username(username)
             .profileImage(profileImage)
