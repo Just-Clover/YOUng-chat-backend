dd63a3ef3da06cb5cd45ddf31994124d61583270
junxtar <aa1870088@naver.com>
2024-02-03 21:58:09 +0900

    [#251] Refactor: Cache 제거

--- src/main/java/com/clover/youngchat/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/com/clover/youngchat/domain/chatroom/service/ChatRoomService.java
@@ -1,7 +1,6 @@
 package com.clover.youngchat.domain.chatroom.service;
 
 
-import static com.clover.youngchat.domain.chatroom.constant.ChatRoomConstant.CHAT_ROOM_CACHE;
 import static com.clover.youngchat.domain.chatroom.constant.ChatRoomConstant.CHAT_ROOM_DETAIL_LIMIT_SIZE;
 import static com.clover.youngchat.domain.chatroom.constant.ChatRoomConstant.CHAT_ROOM_LIMIT_SIZE;
 import static com.clover.youngchat.domain.chatroom.constant.ChatRoomConstant.COUNT_ONE_FRIEND;
@@ -36,7 +35,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -128,7 +126,7 @@ public class ChatRoomService {
     }
 
     @Transactional(readOnly = true)
-    @Cacheable(cacheNames = CHAT_ROOM_CACHE, key = "#chatRoomId.toString().concat(':').concat(#lastChatId)", cacheManager = "cacheManager")
+//    @Cacheable(cacheNames = CHAT_ROOM_CACHE, key = "#chatRoomId.toString().concat(':').concat(#lastChatId)", cacheManager = "cacheManager")
     public ChatRoomPaginationDetailGetRes getPaginationDetailChatRoom(Long chatRoomId,
         Long lastChatId,
         User user) {
