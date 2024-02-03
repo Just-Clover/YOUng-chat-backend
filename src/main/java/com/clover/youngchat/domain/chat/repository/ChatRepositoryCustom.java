b2cead3808e16831d2d2be8bd451da219a1b291f
junxtar <aa1870088@naver.com>
2024-02-03 18:57:10 +0900

    [#251] Refactor: 기존 Slice -> RestSlice 적용

--- src/main/java/com/clover/youngchat/domain/chat/repository/ChatRepositoryCustom.java
+++ src/main/java/com/clover/youngchat/domain/chat/repository/ChatRepositoryCustom.java
@@ -2,12 +2,12 @@ package com.clover.youngchat.domain.chat.repository;
 
 
 import com.clover.youngchat.domain.chat.dto.response.ChatRes;
-import org.springframework.data.domain.Slice;
+import com.clover.youngchat.global.response.RestSlice;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepositoryCustom {
 
-    Slice<ChatRes> findChatsByChatRoomId(Long chatRoomId, Long cursorChatId, int limitSize);
+    RestSlice<ChatRes> findChatsByChatRoomId(Long chatRoomId, Long cursorChatId, int limitSize);
 
 }
